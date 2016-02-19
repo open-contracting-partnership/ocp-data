af1e744b63b1e06a61737c4af8b2e253badc362a
Olaf Veerman <olaf.veerman@gmail.com>
2016-02-19 08:49:16 -0500

    Generate map.json

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+var _ = require('lodash');
 var async = require('async');
 var fs = require('fs-extra');
+var mapData = require('./lib/ne_110m_admin_0_countries.json');
 
 // Merge all individual countries from the OC Status survey in one file
 var sourceDir = './data/oc-status';
 var targetDir = './dist/oc-status';
 var mergedFile = 'all.json';
 var tableFile = 'table.json';
+var mapFile = 'map.json';
 var mergedData = [];
 
 var tableData = {
@@ -50,9 +53,11 @@ function prepTableData (countryData) {
   var d = {
     country: countryData.name,
     ocds_data: processOCDS(countryData),
-    ocds_implementation: countryData.results.ocds_implementation ? 'Yes' : 'No',
-    ogp_commitment: countryData.results.ogp_commitments[0].ogp_commitment !== '' || null ? 'Yes' : 'No'
+    ocds_implementation: countryData.results.ocds_implementation ? 'Yes' : 'No'
   };
+  if (countryData.results.ogp_commitments[0]) {
+    d.ogp_commitment = countryData.results.ogp_commitments[0].ogp_commitment !== '' || null ? 'Yes' : 'No';
+  }
   return d;
 }
 
@@ -71,19 +76,28 @@ async.waterfall([
         var contents = fs.readFileSync(path);
         var jsonData = JSON.parse(contents);
 
+        var i = _.findIndex(mapData.features, function (o) { return o.properties.iso_a2.toLowerCase() === jsonData.iso; });
+        if (i !== -1) {
+          _.merge(mapData.features[i].properties, jsonData.results);
+        }
+
         tableData.data.push(prepTableData(jsonData));
         mergedData.push(jsonData);
       }
-      callback(err, mergedData, tableData);
+      callback(err, mergedData, tableData, mapData);
     });
   },
-  function (mergedData, tableData, callback) {
+  function (mergedData, tableData, mapData, callback) {
     // Write the merged file
     fs.writeFile(`${targetDir}/${mergedFile}`, JSON.stringify(mergedData), function (err) {
       callback(err);
     });
-    // Write the merged file
+    // Write the table file
     fs.writeFile(`${targetDir}/${tableFile}`, JSON.stringify(tableData), function (err) {
+      callback(err);
+    });
+    // Write the map file
+    fs.writeFile(`${targetDir}/${mapFile}`, JSON.stringify(mapData), function (err) {
       callback(err);
     });
   }
