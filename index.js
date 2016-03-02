4bb01d1b018c5b909f0e3a0acee0a7a848fe77cb
Olaf Veerman <olaf.veerman@gmail.com>
2016-03-02 09:11:50 -0500

    Use Topojson instead of Geojson on the _map

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var fs = require('fs-extra');
 var sourceDir = './data/oc-status';
 var targetDir = './dist/oc-status';
 
-var mapData = require('./lib/ne_50m_admin_0_countries.json');
+var mapData = require('./lib/ne_50m_admin_0_countries_topo.json');
 var tableData = {
   meta: {
     display: [
@@ -97,9 +97,9 @@ async.waterfall([
           // False values on booleans count as no data
           jsonData.results.has_data = !(_.every(_.map(jsonData.results), _.isEmpty));
 
-          var i = _.findIndex(output.map.data.features, function (o) { return o.properties.iso_a2.toLowerCase() === jsonData.iso; });
+          var i = _.findIndex(output.map.data.objects.ne_50m_admin_0_countries.geometries, function (o) { return o.properties.iso_a2.toLowerCase() === jsonData.iso; });
           if (i !== -1) {
-            _.merge(output.map.data.features[i].properties, jsonData.results);
+            _.merge(output.map.data.objects.ne_50m_admin_0_countries.geometries[i].properties, jsonData.results);
           }
 
           output.index.data[list[f]] = {name: jsonData.name};
