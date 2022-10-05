d704f7010d9d20c3a251efa55a1824180fff2d72
Ben Everard <hello@beneverard.co.uk>
2022-10-05 14:57:30 +0100

    Formatted index.js

--- index.js
+++ index.js
@@ -14,143 +14,143 @@ var targetDir = './dist/oc-status';
 
 var mapData = require('./lib/ne_50m_admin_0_countries_topo.json');
 var tableData = {
-  meta: {
-    display: [
-      {
-        key: 'country',
-        value: 'Country'
-      },
-      {
-        key: 'ocds_data',
-        value: 'Publishing OCDS data'
-      },
-      {
-        key: 'ocds_implementation',
-        value: 'OCDS implementation'
-      },
-      {
-        key: 'ogp_commitment',
-        value: 'Relevant OGP commitment'
-      }
-    ]
-  },
-  data: []
+    meta: {
+        display: [
+            {
+                key: 'country',
+                value: 'Country'
+            },
+            {
+                key: 'ocds_data',
+                value: 'Publishing OCDS data'
+            },
+            {
+                key: 'ocds_implementation',
+                value: 'OCDS implementation'
+            },
+            {
+                key: 'ogp_commitment',
+                value: 'Relevant OGP commitment'
+            }
+        ]
+    },
+    data: []
 };
 
 var output = {
-  merged: {
-    data: [],
-    file: '_all.json'
-  },
-  index: {
-    data: {},
-    file: '_index.json'
-  },
-  map: {
-    data: mapData,
-    file: '_map.json'
-  },
-  table: {
-    data: tableData,
-    file: '_table.json'
-  }
+    merged: {
+        data: [],
+        file: '_all.json'
+    },
+    index: {
+        data: {},
+        file: '_index.json'
+    },
+    map: {
+        data: mapData,
+        file: '_map.json'
+    },
+    table: {
+        data: tableData,
+        file: '_table.json'
+    }
 };
 
-function processOCDS (countryData) {
-  if (countryData.results.ocds_historic_data && countryData.results.ocds_ongoing_data) {
-    return 'Historic and ongoing';
-  } else if (countryData.results.ocds_historic_data) {
-    return 'Historic';
-  } else if (countryData.results.ocds_ongoing_data) {
-    return 'Ongoing';
-  } else {
-    return 'No';
-  }
+function processOCDS(countryData) {
+    if (countryData.results.ocds_historic_data && countryData.results.ocds_ongoing_data) {
+        return 'Historic and ongoing';
+    } else if (countryData.results.ocds_historic_data) {
+        return 'Historic';
+    } else if (countryData.results.ocds_ongoing_data) {
+        return 'Ongoing';
+    } else {
+        return 'No';
+    }
 }
 
-function prepTableData (countryData) {
-  var d = {
-    country: countryData.name,
-    ocds_data: processOCDS(countryData),
-    ocds_implementation: countryData.results.ocds_implementation ? 'Yes' : 'No'
-  };
-  if (countryData.results.ogp_commitments[0]) {
-    d.ogp_commitment = countryData.results.ogp_commitments[0].ogp_commitment !== '' || null ? 'Yes' : 'No';
-  }
-  return d;
+function prepTableData(countryData) {
+    var d = {
+        country: countryData.name,
+        ocds_data: processOCDS(countryData),
+        ocds_implementation: countryData.results.ocds_implementation ? 'Yes' : 'No'
+    };
+    if (countryData.results.ogp_commitments[0]) {
+        d.ogp_commitment = countryData.results.ogp_commitments[0].ogp_commitment !== '' || null ? 'Yes' : 'No';
+    }
+    return d;
 }
 
 async.waterfall([
-  function (callback) {
-    // Write the original data files
-    fs.copy(sourceDir, targetDir, function (err) {
-      callback(err);
-    });
-  },
-  function (callback) {
-
-		output.map.data.objects.ne_50m_admin_0_countries.geometries = _
-			.map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
-				geo.properties = {
-					iso_a2: geo.properties.ISO_A2,
-					name: geo.properties.NAME,
-				}
-
-				return geo;
-			});
-
-
-    // Process the data
-    fs.readdir(sourceDir, function (err, list) {
-
-			const files = _.chain(list)
-				.filter(filename => filename !== '_index.json')
-				.keyBy()
-				.mapValues(filename => JSON.parse(fs.readFileSync(`${sourceDir}/${filename}`)))
-				.mapValues((jsonData, filename) => {
-
-          // Add an indication if the country has any data reported
-          // False values on booleans count as no data
-          jsonData.results.has_data = !(_.every(_.map(jsonData.results), _.isEmpty));
-
-          var geoIndex = _.findIndex(output.map.data.objects.ne_50m_admin_0_countries.geometries, function (o) {
-						 return o.properties.iso_a2.toLowerCase() === jsonData.iso;
-					});
-
-          if (geoIndex !== -1) {
-            _.merge(output.map.data.objects.ne_50m_admin_0_countries.geometries[geoIndex].properties, jsonData.results);
-					}
-
-          output.index.data[filename] = {name: jsonData.name};
-          output.table.data.data.push(prepTableData(jsonData));
-          output.merged.data.push(jsonData);
-				})
-				.value();
-
-			output.map.data.objects.ne_50m_admin_0_countries.geometries =
-				_.map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
-					if (geo.properties.iso_a2 == -99) {
-						geo.properties.iso_a2 = slugify(geo.properties.name);
-					}
-
-					return geo;
-				});
-
-      callback(err, output);
-    });
-  },
-  function (output, callback) {
-    var tasks = [];
-    _.forEach(output, function (f) {
-      tasks.push(function (cb) {
-        fs.writeFile(`${targetDir}/${f.file}`, JSON.stringify(f.data), function (err) {
-          cb(err);
+    function (callback) {
+        // Write the original data files
+        fs.copy(sourceDir, targetDir, function (err) {
+            callback(err);
         });
-      });
-    });
-    async.parallel(tasks, callback);
-  }
+    },
+    function (callback) {
+        // remove unused geojson properties
+        output.map.data.objects.ne_50m_admin_0_countries.geometries = _
+            .map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
+                geo.properties = {
+                    iso_a2: geo.properties.ISO_A2,
+                    name: geo.properties.NAME,
+                }
+
+                return geo;
+            });
+
+        // process country data files
+        fs.readdir(sourceDir, function (err, list) {
+            const files = _.chain(list)
+                .filter(filename => filename !== '_index.json')
+                .keyBy()
+                .mapValues(filename => JSON.parse(fs.readFileSync(`${sourceDir}/${filename}`)))
+                .mapValues((jsonData, filename) => {
+                    // Add an indication if the country has any data reported
+                    // False values on booleans count as no data
+                    jsonData.results.has_data = !(_.every(_.map(jsonData.results), _.isEmpty));
+
+                    var geoIndex = _.findIndex(output.map.data.objects.ne_50m_admin_0_countries.geometries, function (o) {
+                        return o.properties.iso_a2.toLowerCase() === jsonData.iso;
+                    });
+
+                    if (geoIndex !== -1) {
+                        _.merge(output.map.data.objects.ne_50m_admin_0_countries.geometries[geoIndex].properties, jsonData.results);
+                    }
+
+                    output.index.data[filename] = { name: jsonData.name };
+                    output.table.data.data.push(prepTableData(jsonData));
+                    output.merged.data.push(jsonData);
+                })
+                .value();
+
+            // for "countries" with no valid ISO_A2 code, change for a slug of
+            // the name, this prevents any duplicate entries
+
+            output.map.data.objects.ne_50m_admin_0_countries.geometries =
+                _.map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
+                    if (geo.properties.iso_a2 == -99) {
+                        geo.properties.iso_a2 = slugify(geo.properties.name);
+                    }
+
+                    return geo;
+                });
+
+            callback(err, output);
+        });
+    },
+    function (output, callback) {
+        var tasks = [];
+        _.forEach(output, function (f) {
+            tasks.push(function (cb) {
+                fs.writeFile(`${targetDir}/${f.file}`, JSON.stringify(f.data), function (err) {
+                    cb(err);
+                });
+            });
+        });
+        async.parallel(tasks, callback);
+    }
 ], function (err) {
-  if (err) console.error(err.message);
-  console.log('Success, all data processed and ready to serve beautiful things.');
+    if (err) console.error(err.message);
+    console.log('Success, all data processed and ready to serve beautiful things.');
 });
