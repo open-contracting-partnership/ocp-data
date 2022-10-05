a3ef3b170ebf89883f2015cfbae0c0246bd83abe
Ben Everard <hello@beneverard.co.uk>
2022-10-05 14:54:29 +0100

    Improved map data build process

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 var _ = require('lodash');
 var async = require('async');
 var fs = require('fs-extra');
+var slugify = require('slugify')
 
 var sourceDir = './data/oc-status';
 var targetDir = './dist/oc-status';
@@ -87,26 +88,54 @@ async.waterfall([
     });
   },
   function (callback) {
+
+		output.map.data.objects.ne_50m_admin_0_countries.geometries = _
+			.map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
+				geo.properties = {
+					iso_a2: geo.properties.ISO_A2,
+					name: geo.properties.NAME,
+				}
+
+				return geo;
+			});
+
+
     // Process the data
     fs.readdir(sourceDir, function (err, list) {
-      for (var f in list) {
-        if (list[f] !== '_index.json') {
-          var jsonData = JSON.parse(fs.readFileSync(`${sourceDir}/${list[f]}`));
+
+			const files = _.chain(list)
+				.filter(filename => filename !== '_index.json')
+				.keyBy()
+				.mapValues(filename => JSON.parse(fs.readFileSync(`${sourceDir}/${filename}`)))
+				.mapValues((jsonData, filename) => {
 
           // Add an indication if the country has any data reported
           // False values on booleans count as no data
           jsonData.results.has_data = !(_.every(_.map(jsonData.results), _.isEmpty));
 
-          var i = _.findIndex(output.map.data.objects.ne_50m_admin_0_countries.geometries, function (o) { return o.properties.iso_a2.toLowerCase() === jsonData.iso; });
-          if (i !== -1) {
-            _.merge(output.map.data.objects.ne_50m_admin_0_countries.geometries[i].properties, jsonData.results);
-          }
+          var geoIndex = _.findIndex(output.map.data.objects.ne_50m_admin_0_countries.geometries, function (o) {
+						 return o.properties.iso_a2.toLowerCase() === jsonData.iso;
+					});
 
-          output.index.data[list[f]] = {name: jsonData.name};
+          if (geoIndex !== -1) {
+            _.merge(output.map.data.objects.ne_50m_admin_0_countries.geometries[geoIndex].properties, jsonData.results);
+					}
+
+          output.index.data[filename] = {name: jsonData.name};
           output.table.data.data.push(prepTableData(jsonData));
           output.merged.data.push(jsonData);
-        }
-      }
+				})
+				.value();
+
+			output.map.data.objects.ne_50m_admin_0_countries.geometries =
+				_.map(output.map.data.objects.ne_50m_admin_0_countries.geometries, geo => {
+					if (geo.properties.iso_a2 == -99) {
+						geo.properties.iso_a2 = slugify(geo.properties.name);
+					}
+
+					return geo;
+				});
+
       callback(err, output);
     });
   },
