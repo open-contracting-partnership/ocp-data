b6a185ac21a1d5f1ff6c90f7207848c0f852de5d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-02-24 15:31:05 -0500

    build: Upgrade to Node 24 and remove unnecessary dependencies. Output is same as before.

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * used more easily by other applications.
  */
 
-import async from 'async';
-import fs from 'fs-extra';
-import _ from 'lodash';
+import { readFileSync } from 'node:fs';
+import fs from 'node:fs/promises';
 import slugify from 'slugify';
 import mapData from './lib/ne_50m_admin_0_countries_topo.json' with { type: 'json' };
 
@@ -78,88 +77,57 @@ function prepTableData(countryData) {
     return d;
 }
 
-async.waterfall(
-    [
-        (callback) => {
-            // Write the original data files
-            fs.copy(sourceDir, targetDir, (err) => {
-                callback(err);
-            });
-        },
-        (callback) => {
-            // remove unused geojson properties
-            output.map.data.objects.ne_50m_admin_0_countries.geometries = _.map(
-                output.map.data.objects.ne_50m_admin_0_countries.geometries,
-                (geo) => {
-                    geo.properties = {
-                        iso_a2: geo.properties.iso_a2,
-                        name: geo.properties.name,
-                    };
+try {
+    // Write the original data files
+    await fs.cp(sourceDir, targetDir, { recursive: true });
 
-                    return geo;
-                },
+    // remove unused geojson properties
+    output.map.data.objects.ne_50m_admin_0_countries.geometries =
+        output.map.data.objects.ne_50m_admin_0_countries.geometries.map((geo) => {
+            geo.properties = {
+                iso_a2: geo.properties.iso_a2,
+                name: geo.properties.name,
+            };
+            return geo;
+        });
+
+    // process country data files
+    const list = await fs.readdir(sourceDir);
+    for (const filename of list.filter((f) => f !== '_index.json')) {
+        const jsonData = JSON.parse(readFileSync(`${sourceDir}/${filename}`));
+
+        // Add an indication if the country has any data reported
+        jsonData.results.has_data = !Object.values(jsonData.results).every((v) => !v.length);
+
+        const geoIndex = output.map.data.objects.ne_50m_admin_0_countries.geometries.findIndex(
+            (o) => o.properties.iso_a2.toLowerCase() === jsonData.iso,
+        );
+
+        if (geoIndex !== -1) {
+            Object.assign(
+                output.map.data.objects.ne_50m_admin_0_countries.geometries[geoIndex].properties,
+                jsonData.results,
             );
+        }
 
-            // process country data files
-            fs.readdir(sourceDir, (err, list) => {
-                _.chain(list)
-                    .filter((filename) => filename !== '_index.json')
-                    .keyBy()
-                    .mapValues((filename) => JSON.parse(fs.readFileSync(`${sourceDir}/${filename}`)))
-                    .mapValues((jsonData, filename) => {
-                        // Add an indication if the country has any data reported
-                        // False values on booleans count as no data
-                        jsonData.results.has_data = !_.every(_.map(jsonData.results), _.isEmpty);
+        output.index.data[filename] = { name: jsonData.name };
+        output.table.data.data.push(prepTableData(jsonData));
+        output.merged.data.push(jsonData);
+    }
 
-                        const geoIndex = _.findIndex(
-                            output.map.data.objects.ne_50m_admin_0_countries.geometries,
-                            (o) => o.properties.iso_a2.toLowerCase() === jsonData.iso,
-                        );
+    // for "countries" with no valid ISO_A2 code, change for a slug of
+    // the name, this prevents any duplicate entries
+    output.map.data.objects.ne_50m_admin_0_countries.geometries =
+        output.map.data.objects.ne_50m_admin_0_countries.geometries.map((geo) => {
+            if (geo.properties.iso_a2 === -99) {
+                geo.properties.iso_a2 = slugify(geo.properties.name);
+            }
+            return geo;
+        });
 
-                        if (geoIndex !== -1) {
-                            _.merge(
-                                output.map.data.objects.ne_50m_admin_0_countries.geometries[geoIndex].properties,
-                                jsonData.results,
-                            );
-                        }
-
-                        output.index.data[filename] = { name: jsonData.name };
-                        output.table.data.data.push(prepTableData(jsonData));
-                        output.merged.data.push(jsonData);
-                    })
-                    .value();
-
-                // for "countries" with no valid ISO_A2 code, change for a slug of
-                // the name, this prevents any duplicate entries
-
-                output.map.data.objects.ne_50m_admin_0_countries.geometries = _.map(
-                    output.map.data.objects.ne_50m_admin_0_countries.geometries,
-                    (geo) => {
-                        if (geo.properties.iso_a2 === -99) {
-                            geo.properties.iso_a2 = slugify(geo.properties.name);
-                        }
-
-                        return geo;
-                    },
-                );
-
-                callback(err, output);
-            });
-        },
-        (output, callback) => {
-            const tasks = [];
-            _.forEach(output, (f) => {
-                tasks.push((cb) => {
-                    fs.writeFile(`${targetDir}/${f.file}`, JSON.stringify(f.data), (err) => {
-                        cb(err);
-                    });
-                });
-            });
-            async.parallel(tasks, callback);
-        },
-    ],
-    (err) => {
-        if (err) console.error(err.message);
-        console.log('Success, all data processed and ready to serve beautiful things.');
-    },
-);
+    await Promise.all(
+        Object.values(output).map((f) => fs.writeFile(`${targetDir}/${f.file}`, JSON.stringify(f.data))),
+    );
+} catch (err) {
+    console.error(err.message);
+}
