1b5ef72a21e50b8cede8b5091a20fb88fa881c0c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-02-24 14:50:50 -0500

    chore: Align stylistic with biome

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,14 @@
 import neostandard from 'neostandard';
 
-export default neostandard({
-    semi: true,
-});
+export default [
+    ...neostandard({
+        semi: true,
+    }),
+    {
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/space-before-function-paren': ['error', 'never'],
+            '@stylistic/comma-dangle': ['error', 'always-multiline'],
+        },
+    },
+];
